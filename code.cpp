6c26c2a87134d0ffdec2b215d48b767db5dfeb0e
Nikhal Singh <nsingh2_be18@thapar.edu>
2021-07-15 22:22:29 +0530

    add getValue function

--- code.cpp
+++ code.cpp
@@ -30,16 +30,39 @@ public:
 	}
 
 	void insertKeyValue(string key, int value){
-		// ...
+		// ... 2 cases
 		if(m.count(key) != 0){
 			auto it = m[key];
 			it->value = value;
 		}
+		else{
+			// check if cache is full or not 
+			// remove the least recently used item from cache
+			if(l.size() == maxSize){
+				Node last = l.back();
+				m.erase(last.key); // removes from hashmap
+				l.pop_back(); // removes from linkedlist
+			}
+			Node n(key,value);
+			l.push_front(n);
+			m.insert({key, l.begin()});
+		}
 
 	}
 
-	int getValue(string key){
+	int* getValue(string key){
 		// ...
+		if(m.count(key) != 0){
+			auto it = m[key];
+
+			int value = it->value;
+			l.push_front(*it);
+			l.erase(it);
+			m.insert({key, l.begin()});
+
+			return &l.begin()->value;
+		}
+		return NULL;
 
 	}
 
